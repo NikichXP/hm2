4fec4cfa2fe3e4d6eb20ab3e60caa8835c6a541f
NikichXP <nikichx2@gmail.com>
2016-12-25 05:24:04 +0200

    Testing

--- src/main/java/com/hm/api/FileAPI.java
+++ src/main/java/com/hm/api/FileAPI.java
@@ -1,11 +1,30 @@
 package com.hm.api;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 
 @RestController
 @RequestMapping("/file")
 public class FileAPI {
+
+	@RequestMapping("/get/{userId}/{fileId}")
+	private void getFile (HttpServletResponse response, HttpServletRequest request, @PathVariable("userId") String userId,
+	                      @PathVariable("fileId") String fileId) throws Exception {
+		File file = new File("/"+userId + "/" + fileId);
+		ServletContext sc = request.getSession().getServletContext();
+		response.reset();
+		response.setContentType(sc.getMimeType(file.getName()));
+		response.setContentLength((int) file.length());
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + file.getName() + "\"");
+		org.springframework.util.FileCopyUtils.copy(new FileInputStream(file), response.getOutputStream());
+	}
 
 //	private final StorageService storageService;
 //
--- src/main/java/com/hm/manualdb/ConnectionHandler.java
+++ src/main/java/com/hm/manualdb/ConnectionHandler.java
@@ -10,8 +10,7 @@ public class ConnectionHandler {
 	private static MongoDatabase db;
 
 	static {
-		MongoClientURI mongoClientURI;
-		mongoClientURI = new MongoClientURI("mongodb://heroku_6hlqqwnw:amm1faupj3i2th3edirt0lpl8k@ds119568.mlab.com:19568/heroku_6hlqqwnw");
+		MongoClientURI mongoClientURI = new MongoClientURI("mongodb://heroku_6hlqqwnw:amm1faupj3i2th3edirt0lpl8k@ds119568.mlab.com:19568/heroku_6hlqqwnw");
 		client = new MongoClient(mongoClientURI);
 		db = client.getDatabase("heroku_6hlqqwnw");
 	}
