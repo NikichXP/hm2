223d112b79eeedec2575ebd62f84c8d378f1aed6
DmitriStadnik <dmitri.stadnik@gmail.com>
2017-01-23 02:26:12 +0200

    search ready

--- src/main/resources/static/js/j_offers.js
+++ src/main/resources/static/js/j_offers.js
@@ -9,25 +9,86 @@ $(function(){
     offerPage = 0 + offerPage;
     
     var pageOffset = 0;
+    var itemsLimit = 20;
     
     for (var i = 1; i < offerPage; i++) { 
-        pageOffset += 20;    
+        pageOffset += itemsLimit;    
     }
     
 
     var maxPages;  // get number of pages
     
     
+    //Filling search dropdowns
+    $.ajax({
+        type: 'GET',
+        url: currentSite + '/config/list/city',
+        success: function(resData) {
+            $('.dropdown-menu__city').html("");
+            
+            for (var i = 0; i < resData.length; i++)
+            {          
+                $('.dropdown-menu__city').append("<li><a class='dropdown-menu__item' href='#'>" 
+                                            + resData[i]  
+                                            + "</a></li>");  	
+            };                
+                
+        },
+    });
+    
+    $.ajax({
+        type: 'GET',
+        url: currentSite + '/config/list/offer/group',
+        success: function(resData) {
+            $('.dropdown-menu__group').html("");
+            
+            for (var i = 0; i < resData.length; i++)
+            {          
+                $('.dropdown-menu__group').append("<li><a class='dropdown-menu__item' href='#'>" 
+                                            + resData[i]  
+                                            + "</a></li>");  	
+            };                
+                
+        },
+    });
+    
+    $('body').on('click', 'a.dropdown-menu__item', function() {	
+        var cat = $(this).html();
+        
+        $.ajax({
+            type: 'GET',
+            url: currentSite + '/config/list/offer/genre/' + cat,
+            success: function(resData) {
+                $('.dropdown-menu__genre').html("");
+
+                for (var i = 0; i < resData.length; i++)
+                {          
+                    $('.dropdown-menu__genre').append("<li><a class='dropdown-menu__item' href='#'>" 
+                                                + resData[i]  
+                                                + "</a></li>");  	
+                };                
+
+            },
+        });
+
+    });
+       
+    
+    
+    
+    //End of filling search dropdowns
+    
+    
     $.ajax({
         type: 'GET',
         url: currentSite + '/product/offer',
-        data: { limit: '20', offset: pageOffset },
+        data: { limit: itemsLimit, offset: pageOffset },
         success: function(resData) {
             $('.offers-container').html("");
             
-            maxPages = Math.ceil(resData[0]/20);
+            maxPages = Math.ceil(resData[0]/itemsLimit);
                     
-            for (var i = 1; i < Math.ceil(resData[0]/20) + 1; i++) { 
+            for (var i = 1; i < Math.ceil(resData[0]/itemsLimit) + 1; i++) { 
                 $('.page-navigation__list').append("<li class='page-navigation__page page-navigation__page-num'>" 
                                                 + i
                                              + "</li>");	    
@@ -104,10 +165,95 @@ $(function(){
     });
     
     
+    $('body').on('click', 'ul.dropdown-menu__city li a.dropdown-menu__item', function() {	   
+        $('span#search-innertext__city').html($(this).html());
+    });
+    
+    $('body').on('click', 'ul.dropdown-menu__group li a.dropdown-menu__item', function() {	   
+        $('span#search-innertext__group').html($(this).html());
+    });
+    
+    $('body').on('click', 'ul.dropdown-menu__genre li a.dropdown-menu__item', function() {	   
+        $('span#search-innertext__genre').html($(this).html());
+    });
+    
+    
+    
+    $('body').on('click', '#offer-search', function() {	   
+        
+        var sendData = { limit: itemsLimit, 
+                        offset: pageOffset, 
+                      }
+        if ($('#search-innertext__city').html() != 'Город') sendData.city = $('#search-innertext__city').html();
+        if ($('#search-innertext__genre').html() != 'Жанр') sendData.genre = $('#search-innertext__genre').html();
+        if ($('#search-innertext__group').html() != 'Услуга') sendData.group = $('#search-innertext__group').html();
+        
+        $.ajax({
+            type: 'GET',
+            url: currentSite + '/product/offer',
+            data: sendData,
+            success: function(resData) {
+                $('.offers-container').html("");
+                $('.page-navigation__list').html("");
+
+                maxPages = Math.ceil(resData[0]/itemsLimit);
+                $('.page-navigation__list').append("<li class='page-navigation__page page-navigation__page-first'>Первая</li>");		
+                $('.page-navigation__list').append("<li class='page-navigation__page page-navigation__page-prev'>Предыдущая</li>");
+
+                for (var i = 1; i < Math.ceil(resData[0]/itemsLimit) + 1; i++) { 
+                    $('.page-navigation__list').append("<li class='page-navigation__page page-navigation__page-num'>" 
+                                                    + i
+                                                 + "</li>");	    
+                }
+
+                $('.page-navigation__list').append("<li class='page-navigation__page' id='page-navigation__page-next'>" +
+                            "Следующая" +
+                            "</li>");
+                $('.page-navigation__list').append("<li class='page-navigation__page' id='page-navigation__page-last'>" +
+                            "Последняя" +
+                            "</li>");
+
+                for (var i = 1; i < resData.length; i++)
+                {          
+                    $('.offers-container').append("<div class='col-md-3 col-sm-6 hero-feature'>" 
+                                                + "<img src='" + currentSite + "/file/get/" + resData[i].validImage + "' alt=''>" 
+                                                + "<div class='prob-block-bg'>"
+                                                + "</div>"
+                                                + "<div class='prob-block-desc'>"
+                                                    + "<h5>г. " + resData[i].city + "</h5>"
+                                                    + "<h4>" + resData[i].title + "</h4>"
+                                                + "</div>"
+                                                + "<div class='prob-block-price'> "
+                                                    + "<div class='price-new'>" + resData[i].finalPrice + " грн</div>"
+                                                    + "<div class='price-old'>" + resData[i].price + " грн</div>"
+                                                    + "<div class='price-to'>" + i + " дней</div>"
+                                                + "</div>"
+                                                + "<div class='prob-block-dis'>-" + resData[i].discount + "%</div>"
+                                                + "</div>");  	
+                };                
+
+            },
+        });
+        
+    });
+    
+    
+    
+    //Calendar
     
 
+    $('.button-date').dcalendarpicker({
+        format: 'dd-mm-yyyy'
+    });
     
-    
+    $('body').on('click', 'td.date', function() {	   
+        var day = $(this).children('span').html();
+        var mmyy = $('th#currM').html();
+        $('.button-date').html(day + ' ' + mmyy);
+    });
+
+
+    //End of Calendar
     
     
 
