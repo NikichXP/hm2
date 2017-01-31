93a412c6287e988c7ccd50a15a6228003dd0ddab
NikichXP <nikichx2@gmail.com>
2017-01-31 14:38:09 +0200

    Mywork

--- src/main/resources/static/admin/nik_admin.js
+++ src/main/resources/static/admin/nik_admin.js
@@ -2,16 +2,33 @@
 
 let currentSite = "http://localhost:8080/";
 
+let users = null;
+
 $(function () {
+
     $.ajax({
         type: 'GET',
         url: currentSite + 'api/admin/user/users',
         success: function (response) {
-            response
-                .sort((x1, x2) => x1.name.localeCompare(x2.name))
-                .forEach(x => {
-                $('#user-table-data')
-            .append(`<tr>
+            users = response;
+            print();
+        }
+    });
+
+    $('#usertable.userid').on('click', function () {
+        alert('lol');
+        sort('id');
+    });
+
+    let sort = function (arg) {
+        users = users.sort((x1, x2) => x1.arg.localeCompare(x2.arg));
+        print();
+    };
+
+    let print = function () {
+        $('#user-table-data').remove();
+        users.forEach(x => {
+            $('#user-table-data').append(`<tr>
                 <td>${x.id}</td>
                 <td>${x.name}</td>
                 <td>${x.mail}</td>
@@ -20,6 +37,6 @@ $(function () {
                 <td>${x.regdate}</td>
                 </tr>`);
         });
-        }
-    });
+    };
+
 });
