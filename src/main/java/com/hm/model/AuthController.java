1f8d7106bc881ea3e0e938f273f336fe0dce544e
NikichXP <nikichx2@gmail.com>
2017-02-12 00:37:22 +0200

    fix SOE

--- src/main/java/com/hm/model/AuthController.java
+++ src/main/java/com/hm/model/AuthController.java
@@ -80,6 +80,11 @@ public class AuthController {
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 				return getLoggedToken(sessionId, clazz);
 			}
 		}
