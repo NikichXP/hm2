f824cf2beea91663e3dc756ec7c89e8c1067fe88
DmitriStadnik <dmitri.stadnik@gmail.com>
2017-02-17 03:48:51 +0200

    again

--- src/main/resources/static/js/admin/j_workers.js
+++ src/main/resources/static/js/admin/j_workers.js
@@ -13,15 +13,15 @@ $(function(){
     $('body').on('click', 'th', function() {	   
         
         switch ($(this).attr('class').split('-')[1]) {
-                case 'id': data.sort((e1, e2) => e1.id - e2.id); tableGen(data); break; 
-                case 'name': data.sort((e1, e2) => e1.name.localeCompare(e2.name)); tableGen(data); break; 
-                case 'city': data.sort((e1, e2) => e1.city.localeCompare(e2.city)); tableGen(data); break; 
-                case 'mail': data.sort((e1, e2) => e1.mail.localeCompare(e2.mail)); tableGen(data); break; 
-                case 'prop': data.sort((e1, e2) => e1.profession.localeCompare(e2.profession)); tableGen(data); break; 
-                case 'type': data.sort((e1, e2) => (e2.pro) ? (e2.pro == e1.pro) ? 0 : 1 : (e2.pro == e1.pro) ? 0 : -1); tableGen(data); break; 
-                case 'tel': data.sort((e1, e2) => e1.phone - e2.phone); tableGen(data); break; 
-                case 'acc': data.sort((e1, e2) => (e2.banned) ? (e2.banned == e1.banned) ? 0 : 1 : (e2.banned == e1.banned) ? 0 : -1); tableGen(data); break; 
-                //case 'comm': data.sort((e1, e2) => e1.description - e2.description); tableGen(data); break; 
+                case 'id': data.sort((e1, e2) => e1.id - e2.id); tableGen(data, tableType); break; 
+                case 'name': data.sort((e1, e2) => e1.name.localeCompare(e2.name)); tableGen(data, tableType); break; 
+                case 'city': data.sort((e1, e2) => e1.city.localeCompare(e2.city)); tableGen(data, tableType); break; 
+                case 'mail': data.sort((e1, e2) => e1.mail.localeCompare(e2.mail)); tableGen(data, tableType); break; 
+                case 'prop': data.sort((e1, e2) => e1.profession.localeCompare(e2.profession)); tableGen(data, tableType); break; 
+                case 'type': data.sort((e1, e2) => (e2.pro) ? (e2.pro == e1.pro) ? 0 : 1 : (e2.pro == e1.pro) ? 0 : -1); tableGen(data, tableType); break; 
+                case 'tel': data.sort((e1, e2) => e1.phone - e2.phone); tableGen(data, tableType); break; 
+                case 'acc': data.sort((e1, e2) => (e2.banned) ? (e2.banned == e1.banned) ? 0 : 1 : (e2.banned == e1.banned) ? 0 : -1); tableGen(data, tableType); break; 
+                //case 'comm': data.sort((e1, e2) => e1.description - e2.description); tableGen(data, tableType); break; 
         }
     
 
